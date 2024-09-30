ca7923d86e04043a75bf857c7122f51e67748db7
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-09-30 10:54:25 +0200

    TD4

---
+++ onliner.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+call(int c, char **v)
+{
+    return !m(v[1], v[2]);
+}
+
+m(char *s, char *t)
+{
+    return *t - 42
+               ? *s
+                     ? 63 == *t | *s == *t && m(s + 1, t + 1)
+                     : !*t
+               : m(s, t + 1) || *s && m(s + 1, t);
+}
+
+main(int argc, char **argv)
+{
+    return call(argc, argv);
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# TD4 - Qualité algo
+
+## Physical SLOC
+
+- nombre de lignes
+
+## Local SLOC
+
+- nombre d'instructions (lignes réelles)
